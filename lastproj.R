3e2c52dcf67bf1fedb3ceeb5618e55ed15c0f00b
aileia <nursen.tore@gmail.com>
2020-06-19 00:03:12 +0300

    added doğalgaz info

--- lastproj.R
+++ lastproj.R
@@ -6,6 +6,8 @@ library(tidyr)
 library(lubridate)
 library(ggplot2)
 library(leaflet)
+library(plotly)
+library(readxl)
 #read meteorology data
 met000_file <- paste0("https://data.ibb.gov.tr/dataset/824c942e-4e1b-41b6-8e33-394aae4a55e8/resource/8b8889bc-7d81-4708-9e4d-74e1342c7f8c/download/meteoroloji_000.csv")
 met000 <- read_csv(met000_file)
@@ -65,3 +67,52 @@ leaflet() %>%
   addMarkers(data = dolmer, 
              lng = ~ LONGITUDE, lat = ~ LATITUDE,
              clusterOptions = markerClusterOptions())
+head(dolmer)
+  doalgaztuk <- read_excel('doalgaztuk.xlsx')
+  f1 <- list(size = 8)
+  doalgaztuk%>%
+  gather(key = Yıl, value = Tüketim,-İlçe)%>%
+  plot_ly(
+    x = ~reorder(İlçe,Tüketim), 
+    y = ~Tüketim,  
+    frame = ~Yıl, 
+    hoverinfo = "text",
+    type = 'bar'
+  )%>%
+  layout(
+    title = "İlçelerin yıllara göre doğalgaz tüketimleri",
+    xaxis = list(title = "İlçeler",
+                 tickfont = f1)
+  )
+
+ aksaraymet <- read_excel('otomatik-meteorolojik-gozlem-istasyon-verileri-akom (1).xlsx',3)
+ aksaraymet$Ort_Hava_S<-as.double(aksaraymet$Ort_Hava_S)
+ 
+translatemonth<- data.frame("Ay" = c("Ocak","Şubat","Mart","Nisan","Mayıs",
+                                     "Haziran","Temmuz","Ağustos","Eylül",
+                                     "Ekim","Kasım","Aralık"), 
+                            "Month" = c("January","February","March","April",
+                                        "May","June","July","August","September",
+                                        "October","November","December"),
+                            "Mevsim"=c("Kış","Kış","İlkbahar","İlkbahar","İlkbahar","Yaz","Yaz","Yaz","Sonbahar","Sonbahar","Sonbahar","Kış"))
+aksaraymet
+aksaraymet %>% left_join(translatemonth)%>%
+  unite(datetime, `Verinin Yılı`, Month, Gün)%>%
+  mutate(datetime = ymd(datetime))%>%
+  plot_ly(
+    x = ~datetime, 
+    y = ~Ort_Hava_S,  
+    hoverinfo = "text",
+    type = 'scatter', mode = 'lines'
+  )
+aksaraymet %>% left_join(translatemonth)%>%
+  rename(Yıl=`Verinin Yılı`)%>%
+  group_by(Mevsim,Yıl)%>%
+  summarize(OrtSıcaklık=mean(Ort_Hava_S,na.rm=TRUE))%>%
+  filter(Mevsim=="Kış")%>%
+  plot_ly(
+    x = ~Yıl,
+    y = ~OrtSıcaklık,  
+    hoverinfo = "text",
+    type = 'scatter', mode = 'lines'
+  )
